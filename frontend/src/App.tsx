41873aa169ece5636801c9ce5379050a0ff19a31
ahasan0015 <ahasanstu@gmail.com>
2026-07-02 13:43:31 +0600

    fix app.tsx song table import

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,14 @@
-// import SongTable from './components/pages/SongTable';
+import SongTable from "./components/pages/SongTable";
 
-import SongTable2 from "./components/pages/SongTable";
+
+
 
 
 function App() {
   return (
     <div className="App">
       {/* <SongTable /> */}
-      <SongTable2 />
+      <SongTable />
     </div>
   );
 }
